3d205193bf5fd696e5442146587276b07fead3b5
edd-ie <ogendoeddie@gmail.com>
2024-08-19 01:57:45 -0300

    commit

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const path = "./data.json";
 
 // The commit dates
 
-let pastDaysCount = 20;
+let pastDaysCount = 17;
 let pastDate;
 
 
 
-for (let i = pastDaysCount; i > -1; i--) {
+for (let i = pastDaysCount; i > 0; i--) {
 	pastDate = moment().subtract(i, 'day').format();
 
 	file.writeFile(path, { date: pastDate }, function (err) {
 		if (err) {
 			console.log(err);
 		}
-		console.log({ date: pastDate });
+		console.log(pastDate + " " + i);
 	});
 
-	simpleGit().add([path]).commit(pastDate, { '--date': pastDate }).push;
+	simpleGit().add([path]).commit(pastDate, { '--date': pastDate }).push();
 
 }
 
