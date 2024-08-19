92539649107f7a0d3027112f1e5e5b47afef890b
edd-ie <ogendoeddie@gmail.com>
2024-08-18 22:57:10 -0300

    message

--- index.js
+++ index.js
@@ -3,35 +3,25 @@ const moment = require('moment');
 const simpleGit = require('simple-git');
 
 const path = "./data.json";
-const Date = moment().format();
-const data = {
-	date: Date
-};
-
-file.writeFile(path, data, function (err) {
-	if (err) {
-		console.log(err);
-	}
-});
 
 // The commit dates
 
-let pastDaysCount = 10;
+let pastDaysCount = 9;
 let pastDate;
 
 
 
-for (let i = 0; i < pastDaysCount + 1; i++) {
+for (let i = 1; i < pastDaysCount; i++) {
 	pastDate = moment().subtract(i, 'day').format();
 
 	file.writeFile(path, { date: pastDate }, function (err) {
 		if (err) {
 			console.log(err);
 		}
+		console.log({ date: pastDate });
 	});
 
-	simpleGit().add([path]).commit(
-		pastDate, { '--date': pastDate });
-	console.log(pastDate);
+	simpleGit().add([path]).commit(pastDate, { '--date': pastDate }).push;
+
 }
 
